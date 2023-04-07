e4e9fd82da4df120633f0ff3eec067a3aa68f75a
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-04-07 10:58:35 -0700

    Initial commit

---
+++ Bicycle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1_Classes
+{
+    public class Bicycle
+    {
+        enum TypesOfChains {Chain1, Chain2, Chain3}
+        //Fields
+        string _brakes;
+        TypesOfChains _chain;
+        string _wheelType;
+        int _numberOfGears;
+        string _frame;
+        string _pedals;
+        string _handles;
+        int _numberofSeats;
+        string _uniqueSerial;
+        //Constructor
+        public Bicycle(int numberofSeats)
+        {
+            Random rand = new Random();
+            int unique = rand.Next(10000000, 100000000);
+            _uniqueSerial = unique.ToString(); ;
+            NumberOfSeats = numberofSeats;
+        }
+
+
+
+        //Properties
+        public string UniqueSerial
+        {
+            get => _uniqueSerial;
+        }
+        public int NumberOfSeats
+        {
+            get => _numberofSeats;
+            set
+            {
+                if(value > 0)
+                {
+                    _numberofSeats = value;
+                }
+                else
+                {
+                    Console.WriteLine("Please input correct value");
+                }
+            }
+        }
+        public string Brakes { get => _brakes; set => _brakes = value; }
+        private TypesOfChains Chain { get => _chain; set => _chain = value; }
+        public string WheelType { get => _wheelType; set => _wheelType = value; }
+        public int NumberOfGears { get => _numberOfGears; set => _numberOfGears = value; }
+        public string Frame { get => _frame; set => _frame = value; }
+        public string Pedals { get => _pedals; set => _pedals = value; }
+        public string Handles { get => _handles; set => _handles = value; }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"Serial: {UniqueSerial}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lecture_1_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new instance of Bicycle class
+            Bicycle bicycle = new Bicycle(-1);
+            Bicycle bicycle2 = new Bicycle(-1);
+            Bicycle bicycle3 = bicycle2;
+            Console.WriteLine($"bicycle 1 {bicycle}");
+            Console.WriteLine($"bicycle 2 {bicycle2}");
+            Console.WriteLine($"bicycle 3 {bicycle3}");
+        }
+    }
+}
